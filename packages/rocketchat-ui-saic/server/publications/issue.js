e0f96ae57a98a0277edfe008583dd697dc01867e
yuyixg <yuyixg@gmail.com>
2016-11-25 13:46:22 +0800

    简化代码

--- packages/rocketchat-ui-saic/server/publications/issue.js
+++ packages/rocketchat-ui-saic/server/publications/issue.js
@@ -3,21 +3,23 @@ Meteor.methods(
     'issueInsert': function (Attributes) {
       console.log(Attributes);
 
-       var result = HTTP.call('POST', mmtServerURL+"mmt-web/f/mm/mmtQuestion/saveMmtQuestion",
-          {
-            data: postdata
-          });
-    
+      var result = HTTP.call('POST', mmtServerURL + "mmt-web/f/mm/mmtQuestion/saveMmtQuestion",
+        {
+          data: postdata
+        });
+
     },
     'issueUpdate': function (Attributes) {
       console.log(Attributes);
 
     },
     'issueDelete': function (id) {
-     
+
     },
     'issuefindOne': function (id) {
       console.log(id);
+      var issue = HTTP.call('GET', 'mmt-web/f/mm/mmtQuestion/getMmtQuestion?id=' + id);
+      return issue;
 
     }
   }
@@ -27,22 +29,9 @@ HTTP.methods(
   {
     '/issue/getlist': {
       post: function (data) {
-        console.log(data);
-        var postdata={
-              title: "",
-              page: {
-                "pageNo": "1",
-                "pageSize": "10",
-                "orderBy": "update_date"
-        }};
-        postdata.title=data.title;
-        postdata.page.pageNo=data.offset+1;
-        postdata.page.pageSize=data.limit;
-        console.log(postdata);
-        var rdata = { rows: [{ id: 1, name: "" }], total: 1 };
-        var result = HTTP.call('POST', mmtServerURL+"mmt-web/f/mm/mmtQuestion/queryMmtQuestion",
+        var result = HTTP.call('POST', mmtServerURL + "mmt-web/f/mm/mmtQuestion/queryMmtQuestion",
           {
-            data: postdata
+            data: data
           });
 
         console.log(result);
