bf42511fa2dc28d960545d53bfd4e5bbd025f836
zhufengqin <jerome_zhu@126.com>
2016-12-06 18:37:26 -0800

    add permission

--- packages/rocketchat-ui-saic/server/publications/itinfo.js
+++ packages/rocketchat-ui-saic/server/publications/itinfo.js
@@ -3,7 +3,8 @@ Meteor.methods(
         'getitinfobyid': function (data) {
             //console.log(data);
             var postdata = {
-                id: data.id
+                id: data.id,
+                params: GetUser()
             };
 
             var result = HTTP.call('GET', mmtServerURL + "mmt-web/f/mm/mmtAnnounce/getMmtAnnounce?id=" + data.id, {});
@@ -16,7 +17,8 @@ Meteor.methods(
             var result = HTTP.call('POST', mmtServerURL + "mmt-web/f/mm/mmtQuestion/getQuestionCount",
                 {
                     data: {
-                        userId: userId
+                        userId: userId,
+                        params: GetUser()
                     }
                 });
 
@@ -26,52 +28,20 @@ Meteor.methods(
         'getlist': function (data) {
             var result = HTTP.call('POST', mmtServerURL + "mmt-web/f/mm/mmtAnnounce/queryMmtAnnounce",
                 {
-                    data: data
+                    data: data,
+                    params: GetUser()
                 });
-            //if (result.data.total == 0) {
-            //    result.data.rows = [];
-            //}
+
             //console.log(result.data);
+
+            //if (result.data.total == 0) {
+            //    result.data = _.extend(result.data, {
+            //        rows: []
+            //    });
+            //}
+           
             return result.data;
         }
     }
 )
 
-
-HTTP.methods(
-    {
-        '/itinfo/getlist': {
-            post: function (data) {
-                console.log(data);
-                /*
-                                var postdata = {
-                                    title: data.search,
-                                    page: {
-                                        "pageNo": ,
-                                        "pageSize": data.limit
-                                        //"orderBy": data.orderBy
-                                    }
-                                };
-                
-                                // console.log(postdata);
-                */
-
-                var result = HTTP.call('POST', mmtServerURL + "mmt-web/f/mm/mmtAnnounce/queryMmtAnnounce",
-                    {
-                        data: data
-                    });
-
-                //var  data = [
-                //{ "id": 1, "title": "sallency1", "description": "description description description description description", "updateDate": '2016-11-01' },
-                //{ "id": 2, "title": "sallency2", "description": "description description description description description", "updateDate": '2016-11-02' },
-                //{ "id": 3, "title": "sallency3", "description": "description description description description description", "updateDate": '2016-11-03' },
-                //{ "id": 4, "title": "sallency4", "description": "description description description description description", "updateDate": '2016-11-04' },
-                //{ "id": 5, "title": "sallency5", "description": "description description description description description", "updateDate": '2016-11-05' }];
-
-                //console.log(result);
-                console.log(result.data);
-                return result.data;
-            }
-        }
-    }
-)
